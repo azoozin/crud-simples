77897b3c994c766eb1c309e2410f4ac0eb43b886
azoozin <155941153+azoozin@users.noreply.github.com>
2025-04-11 15:32:04 -0300

    add Form component

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 
 const FormContainer = styled.form`
@@ -12,8 +12,52 @@ const FormContainer = styled.form`
     border-radius: 5px;
 `;
 
-const Form = () => {
-    return <FormContainer></FormContainer>;
+const InputArea = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Input = styled.input`
+    width: 120px;
+    padding: 0 10px;
+    border: 1px solid #bbb;
+    border-radius: 5px;
+    height: 40px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+    border: none;
+    background-color: #2c73d2
+    color: white;
+    height: 42px
+`;
+
+const Label = styled.label``;
+
+const Form = ({ onEdit }) => {
+    const ref = useRef();
+
+    return (
+        <FormContainer ref={ref}>
+            <InputArea>
+                <Label>Produto</Label>
+                <Input name="nome-produto" />
+            </InputArea>
+            <InputArea>
+                <Label>Categoria</Label>
+                <Input name="categoria-produto" />
+            </InputArea>
+            <InputArea>
+                <Label>Quantidade</Label>
+                <Input name="quantidade-produto" />
+            </InputArea>
+
+            <Button type="submit">Salvar</Button>
+        </FormContainer>
+    );
 };
 
 export default Form;
